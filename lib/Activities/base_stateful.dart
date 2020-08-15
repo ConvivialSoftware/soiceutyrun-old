838e1630053143e0252551d23167b9b332e8e968
Kasun Niroshana <zapniroshana420@gmail.com>
2020-08-15 13:43:57 +0530

    --update-- base_stateful.dart

--- lib/Activities/base_stateful.dart
+++ lib/Activities/base_stateful.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:societyrun/GlobalClasses/GlobalVariables.dart';
 import 'package:societyrun/Models/gatepass_payload.dart';
+import 'package:societyrun/Models/gatepass_payload_ios.dart';
 import 'package:societyrun/firebase_notification/firebase_message_handler.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -13,12 +15,23 @@ Map<String, dynamic> receivedMessage;
 
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
   GatePassPayload gatePassPayload;
-  try {
-    String jsonStr = message["data"]["payload"];
-    Map<String, dynamic> temp = json.decode(jsonStr);
-    gatePassPayload = GatePassPayload.fromJson(temp);
-  } catch (e) {
-    print(e);
+  GatePassPayloadIos gatePassPayloadIos;
+  if(Platform.isIOS) {
+    try {
+      String jsonStr = message["payload"];
+      Map<String, dynamic> temp = json.decode(jsonStr);
+      gatePassPayloadIos = GatePassPayloadIos.fromJson(temp);
+    } catch (e) {
+      print(e);
+    }
+  }else{
+    try {
+      String jsonStr = message["data"]["payload"];
+      Map<String, dynamic> temp = json.decode(jsonStr);
+      gatePassPayload = GatePassPayload.fromJson(temp);
+    } catch (e) {
+      print(e);
+    }
   }
 
   int msgId = int.tryParse(message["data"]["msgId"].toString()) ?? 0;
@@ -35,16 +48,22 @@ Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
       playSound: true,
       sound: RawResourceAndroidNotificationSound("noti_ring"),
 
-
-
   );
   var iOSPlatformChannelSpecifics =
   IOSNotificationDetails(presentAlert: true, presentSound: true);
   var platformChannelSpecifics = NotificationDetails(
       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-  flutterLocalNotificationsPlugin.show(msgId, gatePassPayload.title,
-      gatePassPayload.body, platformChannelSpecifics,
-      payload: message['data']['payload']);
+  if(Platform.isAndroid) {
+    flutterLocalNotificationsPlugin.show(msgId, gatePassPayload.title,
+        gatePassPayload.body, platformChannelSpecifics,
+        payload: message['data']['payload']);
+  }else{
+    flutterLocalNotificationsPlugin.show(msgId, gatePassPayloadIos.payload.title,
+        gatePassPayloadIos.payload.body, platformChannelSpecifics,
+        payload: message['payload']);
+  }
+
+
 
   return Future<void>.value();
 }
@@ -109,13 +128,27 @@ abstract class BaseStatefulState<T extends StatefulWidget> extends State<T> {
 
   _showNotification(Map<String, dynamic> message) {
     GatePassPayload gatePassPayload;
-    try {
-      String jsonStr = message["data"]["payload"];
-      Map<String, dynamic> temp = json.decode(jsonStr);
-      gatePassPayload = GatePassPayload.fromJson(temp);
-    } catch (e) {
-      print(e);
+    GatePassPayloadIos gatePassPayloadIos;
+    if(Platform.isIOS) {
+      try {
+        String jsonStr = message["payload"];
+        Map<String, dynamic> temp = json.decode(jsonStr);
+        gatePassPayloadIos = GatePassPayloadIos.fromJson(temp);
+        print(">>>>>>>>>>${gatePassPayloadIos.payload.tYPE}");
+        gatePassPayload = _mapIosPayload(gatePassPayloadIos);
+      } catch (e) {
+        print(e);
+      }
+    }else{
+      try {
+        String jsonStr = message["data"]["payload"];
+        Map<String, dynamic> temp = json.decode(jsonStr);
+        gatePassPayload = GatePassPayload.fromJson(temp);
+      } catch (e) {
+        print(e);
+      }
     }
+
     try {
       _fcm.showAlert(context, gatePassPayload);
     } catch (e) {
@@ -142,8 +175,40 @@ abstract class BaseStatefulState<T extends StatefulWidget> extends State<T> {
         IOSNotificationDetails(presentAlert: true, presentSound: true);
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    flutterLocalNotificationsPlugin.show(msgId, gatePassPayload.title,
-        gatePassPayload.body, platformChannelSpecifics,
-        payload: message['data']['payload']);
+
+    if(Platform.isAndroid) {
+      flutterLocalNotificationsPlugin.show(msgId, gatePassPayload.title,
+          gatePassPayload.body, platformChannelSpecifics,
+          payload: message['data']['payload']);
+    }else{
+      flutterLocalNotificationsPlugin.show(msgId, gatePassPayloadIos.payload.title,
+          gatePassPayloadIos.payload.body, platformChannelSpecifics,
+          payload: message['payload']);
+    }
+
+  }
+
+  GatePassPayload _mapIosPayload(GatePassPayloadIos p){
+
+    GatePassPayload gp;
+    gp.cONTACT = p.payload.cONTACT;
+    gp.iNTIME = p.payload.iNTIME;
+    gp.sound = p.payload.sound;
+    gp.uSERID = p.payload.uSERID;
+    gp.title = p.payload.title;
+    gp.body = p.payload.body;
+    gp.vISITORNAME = p.payload.vISITORNAME;
+    gp.vID = p.payload.vID;
+    gp.iNDATE = p.payload.iNDATE;
+    gp.iMAGE = p.payload.iMAGE;
+    gp.iNBY = p.payload.iNBY;
+    gp.fROMVISITOR = p.payload.fROMVISITOR;
+    gp.nOOFVISITORS = p.payload.nOOFVISITORS;
+    gp.iD = p.payload.iD;
+    gp.dATETIME = p.payload.dATETIME;
+    gp.rEASON = p.payload.rEASON;
+    gp.tYPE = p.payload.tYPE;
+    return gp;
+
   }
 }
